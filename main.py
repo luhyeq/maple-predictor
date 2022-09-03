0d848dcc509306051da32b3b735f7811e730f300
khai <112670162+khai2rich@users.noreply.github.com>
2022-09-03 22:22:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 intents = discord.Intents().all()
 
-bot = commands.Bot(command_prefix=";", intents=intents))
+bot = commands.Bot(command_prefix=";", intents=intents)
 
 bot.remove_command('help')
 
