e569280d901f9e01120276b75f1aa4e7ed0d526b
khai <112670162+khai2rich@users.noreply.github.com>
2022-09-03 22:19:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import datetime
 import time
 import random
 
-bot = commands.Bot(command_prefix=";", intents=discord.Intents.default())
+intents = discord.Intents().all()
+
+bot = commands.Bot(command_prefix=";", intents=intents))
 
 bot.remove_command('help')
 
